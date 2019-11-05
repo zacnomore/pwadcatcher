63d957ae041d396fe4b91aadfe6dc310e172b235
Zachary Svoboda <cczac1@gmail.com>
2019-11-04 21:06:10 -0500

    Menu height

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
@@ -9,7 +10,8 @@ import {MatButtonToggleModule} from '@angular/material/button-toggle';
     CommonModule,
   ],
   exports: [
-    MatButtonToggleModule
+    MatButtonToggleModule,
+    MatButtonModule
   ]
 })
 export class SharedModule { }
