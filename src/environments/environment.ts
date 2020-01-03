70630b925af9a1ce93a90de3d44e6f46746687a8
Zachary Svoboda <cczac1@gmail.com>
2020-01-02 21:18:29 -0500

    Fix env typo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@
 export const environment = {
   production: false,
   // TODO: Figure out CORS if this is too annoying
-  appleSearchUrl: 'https://pwa-podcatcher-nine-black.now.sh//api/search',
-  feedReadUrl: 'https://pwa-podcatcher-nine-black.now.sh//api/feed'
+  appleSearchUrl: 'https://pwa-podcatcher-nine-black.now.sh/api/search',
+  feedReadUrl: 'https://pwa-podcatcher-nine-black.now.sh/api/feed'
 };
