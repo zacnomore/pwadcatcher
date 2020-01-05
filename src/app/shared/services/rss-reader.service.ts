91c41fb3c1cab5632dcad4fbcd0f020e38b33c56
Zachary Svoboda <cczac1@gmail.com>
2020-01-04 20:11:04 -0500

    Simplify rss service, temporarily remove mapping

--- src/app/shared/services/rss-reader.service.ts
+++ src/app/shared/services/rss-reader.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { map, catchError, tap } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
-import { xml2js } from 'xml-js';
 import { of, Observable } from 'rxjs';
 import { IPodcastFeed } from '../models/podcast.model';
 import { EnvironmentService } from 'src/app/environments/environment.service';
@@ -13,19 +12,7 @@ export class RssReaderService {
 
   public readFeed(collectionId: number): Observable<IPodcastFeed | undefined> {
     console.log(collectionId, 'read feed');
-    return this.http.get(`${this.env.env.feedReadUrl}?collectionId=${collectionId}`, { responseType: 'text'}).pipe(
-      map(resp => xml2js(resp)),
-      map(xml => ({
-        episodes: xml.elements[0].elements[0].elements.filter(
-          node => node.name === 'item'
-        ).map(
-          ep => ({
-            title: ep.elements.find(el => el.name === 'title').elements[0].cdata,
-            image: {small: ep.elements.find(el => el.name === 'itunes:image').attributes.href},
-            ...ep
-          })
-        )
-      } as IPodcastFeed)),
+    return this.http.get<IPodcastFeed>(`${this.env.env.feedReadUrl}?collectionId=${collectionId}`).pipe(
       tap(console.log),
       catchError(err => {
         console.error(err);
