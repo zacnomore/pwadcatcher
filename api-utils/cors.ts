fbc3297cac61f81f5c43a99afa8709e5fee0bcf6
Zachary Svoboda <cczac1@gmail.com>
2020-01-05 22:07:58 -0500

    Fix cors util

--- api-utils/cors.ts
+++ api-utils/cors.ts
@@ -2,10 +2,11 @@ import { NowRequest, NowResponse } from '@now/node';
 
 
 export const addCORS = (req: NowRequest, res: NowResponse) => {
-  if (req.headers.referer &&
-    (req.headers.referer.includes('localhost') || req.headers.referer.includes('pwa-podcatcher'))
+  const origin = req.headers.origin;
+  if (origin &&
+    (origin.includes('localhost') || origin.includes('pwa-podcatcher'))
   ) {
     res.setHeader('Vary', 'Origin');
-    res.setHeader('Access-Control-Allow-Origin', req.headers.referer);
+    res.setHeader('Access-Control-Allow-Origin', origin);
   }
 };
