0e52b48d3d1e108e0b4e6450a8900036af8435f7
Zachary Svoboda <cczac1@gmail.com>
2020-01-11 13:21:23 -0500

    Move xml processing to client, timeout issues

--- api/feed.ts
+++ api/feed.ts
@@ -1,7 +1,5 @@
 import { NowRequest, NowResponse } from '@now/node';
 import { get } from 'http';
-import { xml2js, Element as XMLElement } from 'xml-js';
-import { IPodcastFeed, IPodcastEpisode } from '../src/app/shared/models/podcast.model';
 import { addCORS } from '../api-utils/cors';
 
 export default (req: NowRequest, res: NowResponse) => {
@@ -13,41 +11,9 @@ export default (req: NowRequest, res: NowResponse) => {
     let rawData = '';
     rawFeed.on('data', (chunk) => { rawData += chunk; });
     rawFeed.on('end', () => {
-      const { elements: root } = xml2js(rawData) as XMLElement;
-      const rss: XMLElement = (root || []).find(el => el.name === 'rss') || {elements: []};
-      const {elements: channel } = (rss.elements || []).find(el => el.name === 'channel') || { elements: []};
-
-      const episodes: IPodcastEpisode[] | undefined = (channel || []).filter(
-        el => el.name === 'item'
-      ).map(({ elements }) => {
-        const { elements: titleProps } = (elements || []).find(el => el.name === 'title') || {elements: []};
-        const { text: title = '' } = (titleProps || []).find(el => el.text !== undefined) || { text: '' };
-
-        const defaultUrl = { url: '' };
-        const { attributes: enclosure = defaultUrl } = (elements || []).find(el => el.name === 'enclosure') || { attributes: defaultUrl };
-        const audioUrl = (enclosure.url || '').toString();
-
-        // TODO: Include episode image
-
-        return {
-          title: title.toString(),
-          audioUrl
-        };
-      });
-      const noImage = { href: undefined };
-      const { attributes: image } = (channel || []).find(el => el.name === 'itunes:image') || { attributes: noImage };
-
-      const feed: IPodcastFeed = {
-        defaultImage: image && typeof image.href === 'string'  ? {
-          small: {
-            src: image.href
-          }
-        } : undefined,
-        episodes
-      };
 
       addCORS(req, res);
-      res.json(feed);
+      res.send(rawData);
     });
   });
 };
--- src/app/shared/services/rss-reader.service.ts
+++ src/app/shared/services/rss-reader.service.ts
@@ -2,8 +2,10 @@ import { Injectable } from '@angular/core';
 import { map, catchError, tap } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 import { of, Observable } from 'rxjs';
-import { IPodcastFeed } from '../models/podcast.model';
+import { IPodcastFeed, IPodcastEpisode } from '../models/podcast.model';
 import { EnvironmentService } from 'src/app/environments/environment.service';
+import { xml2js, Element as XMLElement } from 'xml-js';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -12,7 +14,42 @@ export class RssReaderService {
 
   public readFeed(feedUrl: string): Observable<IPodcastFeed | undefined> {
     console.log(feedUrl, 'read feed');
-    return this.http.get<IPodcastFeed>(`${this.env.env.feedReadUrl}?xmlUrl=${feedUrl}`).pipe(
+    return this.http.get(`${this.env.env.feedReadUrl}?xmlUrl=${feedUrl}`, {responseType: 'text'}).pipe(
+      map(xml => {
+        const { elements: root } = xml2js(xml) as XMLElement;
+        const rss: XMLElement = (root || []).find(el => el.name === 'rss') || {elements: []};
+        const {elements: channel } = (rss.elements || []).find(el => el.name === 'channel') || { elements: []};
+
+        const episodes: IPodcastEpisode[] | undefined = (channel || []).filter(
+          el => el.name === 'item'
+        ).map(({ elements }) => {
+          const { elements: titleProps } = (elements || []).find(el => el.name === 'title') || {elements: []};
+          const { text: title = '' } = (titleProps || []).find(el => el.text !== undefined) || { text: '' };
+
+          const defaultUrl = { url: '' };
+          const { attributes: enclosure = defaultUrl } = (elements || []).find(el => el.name === 'enclosure') || { attributes: defaultUrl };
+          const audioUrl = (enclosure.url || '').toString();
+
+          // TODO: Include episode image
+
+          return {
+            title: title.toString(),
+            audioUrl
+          };
+        });
+        const noImage = { href: undefined };
+        const { attributes: image } = (channel || []).find(el => el.name === 'itunes:image') || { attributes: noImage };
+
+        const feed: IPodcastFeed = {
+          defaultImage: image && typeof image.href === 'string'  ? {
+            small: {
+              src: image.href
+            }
+          } : undefined,
+          episodes
+        };
+        return feed;
+      }),
       catchError(err => {
         console.error(err);
         return of(undefined);
