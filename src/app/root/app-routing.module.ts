cb72359235b3cc62cfb58cb19dffbc712cd1ada2
Zachary Svoboda <cczac1@gmail.com>
2020-01-27 20:07:29 -0500

    Add todo

--- src/app/root/app-routing.module.ts
+++ src/app/root/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
+// TODO: Make routes navigable even when not loaded into storage
 const routes: Routes = [
   { path: '', loadChildren:  () => import('../subscriptions/subscriptions.module').then(m => m.SubscriptionsModule)},
   { path: 'player', loadChildren:  () => import('../player/player.module').then(m => m.PlayerModule) },
