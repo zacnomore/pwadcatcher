e4116083c176414149c932080f1df499b5f4242b
Zachary Svoboda <cczac1@gmail.com>
2020-01-28 20:30:11 -0500

    Fixed CORS, remove TODO

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,6 @@
 
 export const environment = {
   production: false,
-  // TODO: Figure out CORS if this is too annoying
   appleSearchUrl: 'https://pwa-podcatcher-nine-black.now.sh/api/search',
   feedReadUrl: 'https://pwa-podcatcher-nine-black.now.sh/api/feed'
 };
