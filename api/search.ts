88e1c0487ecbcb303267e44390cf6e26c7f7e049
Zachary Svoboda <cczac1@gmail.com>
2020-01-28 22:03:11 -0500

    Use simple api

--- api/search.ts
+++ api/search.ts
@@ -1,20 +1,14 @@
 import { NowRequest, NowResponse } from '@now/node';
-import { get } from 'http';
 import { addCORS } from '../api-utils/cors';
+import { simpleRequest } from 'api-utils/simple';
 
-export default (req: NowRequest, res: NowResponse) => {
+export default async (req: NowRequest, res: NowResponse) => {
   const {
     query: { term }
   } = req;
 
-  get(`http://itunes.apple.com/search?media=podcast&term=${term}`, searchResp => {
-    let rawData = '';
-    searchResp.on('data', (chunk) => { rawData += chunk; });
-    searchResp.on('end', () => {
-      addCORS(req, res);
-      res.json(JSON.parse(rawData));
-    });
-
-  });
+  const searchResults = await simpleRequest(`http://itunes.apple.com/search?media=podcast&term=${term}`);
+  addCORS(req, res);
+  res.json(JSON.parse(searchResults.rawData));
 };
 
