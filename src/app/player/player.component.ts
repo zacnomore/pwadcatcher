bde2d842be4c8921afc3f716d16b30406f387fe3
Zachary Svoboda <cczac1@gmail.com>
2020-02-03 18:42:47 -0500

    Style player, add image

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AudioPlayerService, IAudioState, PlayerAction } from './services/audio-player.service';
 import { Observable, Subscription, from } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
-import { map, filter, switchMap, tap } from 'rxjs/operators';
+import { map, filter, switchMap, tap, startWith } from 'rxjs/operators';
 import { IPodcastEpisode } from '../shared/models/podcast.model';
 import { PodcastService } from '../shared/services/podcast.service';
 
@@ -10,32 +10,22 @@ import { PodcastService } from '../shared/services/podcast.service';
   templateUrl: './player.component.html',
   styleUrls: ['./player.component.scss']
 })
-export class PlayerComponent implements OnInit, OnDestroy {
+export class PlayerComponent {
   public audioState$: Observable<IAudioState> = this.audio.audioState$;
-  private subscriptions: Subscription[] = [];
+  public activePodcast$: Observable<IPodcastEpisode> = this.activeRoute.paramMap.pipe(
+    map(params => params.get('episode')),
+    filter((key: string | null): key is string => Boolean(key)),
+    map(key => this.podcasts.getEpisode(key)),
+    switchMap(promise => from(promise)),
+    filter((key: IPodcastEpisode | undefined): key is IPodcastEpisode => Boolean(key)),
+    tap(episode => this.audio.updateSource(episode.audioUrl))
+  );
 
   constructor(
     private audio: AudioPlayerService,
     private activeRoute: ActivatedRoute,
-    private podcasts: PodcastService) { }
-
-
-  ngOnInit() {
-    this.subscriptions.push(
-      this.activeRoute.paramMap.pipe(
-        map(params => params.get('episode')),
-        filter((key: string | null): key is string => Boolean(key)),
-        map(key => this.podcasts.getEpisode(key)),
-        switchMap(promise => from(promise)),
-        filter((key: IPodcastEpisode | undefined): key is IPodcastEpisode => Boolean(key)),
-        tap(episode => this.audio.updateSource(episode.audioUrl))
-      ).subscribe()
-    );
-  }
-
-  ngOnDestroy(): void {
-    this.subscriptions.forEach(sub => sub.unsubscribe());
-  }
+    private podcasts: PodcastService
+  ) {}
 
   public togglePlay = (playing: boolean) => this.audio.doAction(playing ? PlayerAction.Pause : PlayerAction.Play);
   public forward = () => this.audio.doAction(PlayerAction.FastForward);
