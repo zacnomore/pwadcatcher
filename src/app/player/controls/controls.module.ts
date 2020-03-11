74b1165580448d4ffdd6d93dbe45baaf6741331c
Zachary Svoboda <cczac1@gmail.com>
2020-03-11 12:18:16 -0400

    Create split

---
+++ src/app/player/controls/controls.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ControlsComponent } from './controls.component';
+
+
+
+@NgModule({
+  declarations: [ControlsComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ControlsModule { }
