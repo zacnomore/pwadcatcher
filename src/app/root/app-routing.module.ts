a9efd72f1740ea42bbebbd18b2082bf89503fef1
Zachary Svoboda <cczac1@gmail.com>
2020-03-13 20:32:54 -0400

    Playlist route

--- src/app/root/app-routing.module.ts
+++ src/app/root/app-routing.module.ts
@@ -6,7 +6,8 @@ const routes: Routes = [
   { path: '', loadChildren:  () => import('../subscriptions/subscriptions.module').then(m => m.SubscriptionsModule)},
   { path: 'player', loadChildren:  () => import('../player/player.module').then(m => m.PlayerModule) },
   { path: 'search', loadChildren:  () => import('../search/search.module').then(m => m.SearchModule)},
-  { path: 'podcast', loadChildren:  () => import('../podcast/podcast.module').then(m => m.PodcastModule)}
+  { path: 'podcast', loadChildren: () => import('../podcast/podcast.module').then(m => m.PodcastModule) },
+  { path: 'playlist', loadChildren:  () => import('../playlist/playlist.module').then(m => m.PlaylistModule)}
 ];
 
 @NgModule({
