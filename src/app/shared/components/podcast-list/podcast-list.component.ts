412bf8f6c40eb46dc96023d6fe2563e062e9352d
Zachary Svoboda <cczac1@gmail.com>
2020-03-14 14:12:44 -0400

    List reorder logic

--- src/app/shared/components/podcast-list/podcast-list.component.ts
+++ src/app/shared/components/podcast-list/podcast-list.component.ts
@@ -10,14 +10,28 @@ export class PodcastListComponent {
   @Input() list?: IListItem[];
   @Input() draggable?: boolean;
   @Output() clickItem = new EventEmitter<number>();
-  @Output() reorder = new EventEmitter<[]>();
+  @Output() reorder = new EventEmitter<ReorderedItem[]>();
 
-  public onClickItem(index: number) {
+  public onClickItem(index: number): void {
     this.clickItem.emit(index);
+  }
+
+  public drop(reorderedList: IListItem[]): void {
+    this.reorder.emit(reorderedList.map(item => {
+      const originalIndex = this.list?.findIndex(old => old === item);
+      if (originalIndex === undefined) {
+        throw new Error('Item can\'t be found in list during reorder');
+      }
+      return { originalIndex };
+    }));
   }
 }
 
 export interface IListItem {
   title: string;
   image?: IImage;
+}
+
+export interface ReorderedItem {
+  originalIndex: number;
 }
