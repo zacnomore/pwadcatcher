f27a7c199f2121a4f4c2e45f004e493693e9d9bc
Zachary Svoboda <cczac1@gmail.com>
2020-03-14 19:52:47 -0400

    Change playlist button to pause when not playing

--- src/app/playlist/playlist.component.ts
+++ src/app/playlist/playlist.component.ts
@@ -3,6 +3,7 @@ import { PlaylistService } from './services/playlist.service';
 import { map } from 'rxjs/operators';
 import { IListItem, ReorderedItem } from '../shared/components/podcast-list/podcast-list.component';
 import { Observable, combineLatest } from 'rxjs';
+import { AudioPlayerService } from '../player/services/audio-player.service';
 
 @Component({
   selector: 'app-playlist',
@@ -10,19 +11,26 @@ import { Observable, combineLatest } from 'rxjs';
   styleUrls: ['./playlist.component.scss']
 })
 export class PlaylistComponent {
-  public playlist$: Observable<IListItem[]> = combineLatest([this.playlistService.playlist$, this.playlistService.currentEpisode$]).pipe(
-    map(([episodes, current]) => episodes.map(episode => {
+  public playlist$: Observable<IListItem[]> = combineLatest(
+    [this.playlistService.playlist$, this.playlistService.currentEpisode$, this.audioService.audioState$]
+  ).pipe(
+    map(([episodes, current, audio]) => episodes.map(episode => {
     const item: IListItem = {
         title: episode.title,
         image: episode.thumbnail?.small,
-        icon: episode === current ? 'play_arrow' : undefined
+      icon: episode === current ?
+        audio.isPlaying ? 'play_arrow' : 'pause'
+        : undefined
       };
 
       return item;
     }))
   );
 
-  constructor(private playlistService: PlaylistService) {}
+  constructor(
+    private playlistService: PlaylistService,
+    private audioService: AudioPlayerService
+  ) { }
 
   public reorderPlaylist(reorderedItem: ReorderedItem) {
     this.playlistService.reorder(reorderedItem.previousIndex, reorderedItem.currentIndex);
