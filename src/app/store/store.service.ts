0b8bf3ebd4fe2da7c719daef751bef7ef9cf1b25
Zac Svoboda <cczac1@gmail.com>
2020-06-13 21:38:39 -0400

    Move persistence to its own file

--- src/app/store/store.service.ts
+++ src/app/store/store.service.ts
@@ -1,16 +1,15 @@
 import { Injectable } from '@angular/core';
 import { IPodcast, IPodcastEpisode } from '../shared/models/podcast.model';
-import { get as getIDB, set as setIDB, Store} from 'idb-keyval';
-import { debounce } from '../shared/utils';
 import { ISubscription } from '../subscriptions/subscriptions.service';
 import { ISerializablePlaylist } from '../playlist/services/playlist.service';
+import { PersistentStore } from './persistent-store';
 
 // Doesn't need to be an `object` as it could be a string or number
 // tslint:disable-next-line: ban-types
 export interface IStorable { [key: string]: Object | undefined | null; }
 export type ISetterConfig<T> = ISetterConfigUnHashed<T> | ISetterConfigPreHashed<T>;
-export interface ISetterConfigUnHashed<T> { store: PwaStore<T>; keyableProperty: keyof T; }
-export interface ISetterConfigPreHashed<T> { store: PwaStore<T>; prekeyedProperty: keyof T; }
+export interface ISetterConfigUnHashed<T> { store: PersistentStore<T>; keyableProperty: keyof T; }
+export interface ISetterConfigPreHashed<T> { store: PersistentStore<T>; prekeyedProperty: keyof T; }
 const isPrehashed = <T>(config: ISetterConfig<T>): config is ISetterConfigPreHashed<T> =>
   !!(config as ISetterConfigPreHashed<T>).prekeyedProperty;
 
@@ -18,10 +17,10 @@ const isPrehashed = <T>(config: ISetterConfig<T>): config is ISetterConfigPreHas
   providedIn: 'root'
 })
 export class StoreService {
-  private podcasts = new PwaStore<IPodcast>('podcasts');
-  private episodes = new PwaStore<IPodcastEpisode>('episodes');
-  private playlist = new PwaStore<ISerializablePlaylist>('playlist');
-  private subscriptions = new PwaStore<ISubscription>('subscriptions');
+  private podcasts = new PersistentStore<IPodcast>('podcasts');
+  private episodes = new PersistentStore<IPodcastEpisode>('episodes');
+  private playlist = new PersistentStore<ISerializablePlaylist>('playlist');
+  private subscriptions = new PersistentStore<ISubscription>('subscriptions');
 
   public addEpisode = this.createSetter({ store: this.episodes, keyableProperty: 'audioUrl' });
   public getEpisode = this.createGetter(this.episodes);
@@ -64,17 +63,17 @@ export class StoreService {
     }
   }
 
-  private createGetter<T>(store: PwaStore<T>): (K: string) => Promise<(T | undefined)> {
+  private createGetter<T>(store: PersistentStore<T>): (K: string) => Promise<(T | undefined)> {
     return (key: string) => store.get(key);
   }
 
-  private createDeleter<T>(store: PwaStore<T>): (K: string) => void {
+  private createDeleter<T>(store: PersistentStore<T>): (K: string) => void {
     return (key: string) => {
       store.set(key, undefined);
     };
   }
 
-  private createCollector<T>(store: PwaStore<T>): () => Promise<T[]> {
+  private createCollector<T>(store: PersistentStore<T>): () => Promise<T[]> {
     return () => store.getAll();
   }
 
@@ -87,80 +86,5 @@ export class StoreService {
         return hash & hash;
       }, 0)
     ).toString();
-  }
-}
-
-interface ISerializableKeyValuePair<V> {
-  key: string;
-  value: V;
-}
-
-interface ISerializedMap<V> {
-  entries: ISerializableKeyValuePair<V>[];
-}
-class PwaStore<V> {
-  private configuredStore = new Store('pwa-podcatcher');
-  private innerStore = new Map<string, V>();
-  private triggerStorage = debounce(() => setIDB(this.idbKey, this.serialize(this.innerStore), this.configuredStore));
-  private initialized: Promise<void>;
-  private markInitialization = () => { console.error('Intialization failed.'); };
-
-  constructor(private idbKey: string) {
-    this.initialized = new Promise((resolve) => { this.markInitialization = resolve; });
-    getIDB(idbKey, this.configuredStore).then(store => {
-      if (store && this.isStore(store)) {
-        // TODO: Figure out if we can string together some kind of type checking here
-        const deserialized = this.deserialize(store as ISerializedMap<V>);
-
-        if (deserialized) {
-          this.innerStore = (deserialized as Map<string, V>);
-        }
-      }
-      this.markInitialization();
-    }).catch(e => {
-      console.warn('Store does not exist');
-    });
-  }
-
-  public async get(key: string): Promise<V | undefined> {
-    await this.initialized;
-    return this.innerStore.get(key);
-  }
-
-  public async getAll(): Promise<V[]> {
-    await this.initialized;
-    return Array.from(this.innerStore.values());
-  }
-
-  public set(key: string, value: V | undefined): string {
-    if (value !== undefined) {
-      // TODO: Handle collisions
-      this.innerStore.set(key, value);
-    } else {
-      this.innerStore.delete(key);
-    }
-
-    this.triggerStorage();
-
-    return key;
-  }
-
-  private isStore(s: unknown): s is ISerializedMap<unknown> {
-    return Boolean((s as ISerializedMap<unknown>).entries);
-  }
-
-  private serialize(m: Map<string, V>): ISerializedMap<V> {
-    return Array.from(m.entries()).reduce((acc: ISerializedMap<V>, [key, value]: [string, V]) =>
-      ({ entries: [...acc.entries, { key, value }] }), {
-      entries: [] as ISerializableKeyValuePair<V>[]
-    });
-  }
-
-  private deserialize(serialized: ISerializedMap<V>): Map<string, V> {
-    const map = new Map<string, V>();
-    serialized.entries.forEach(({ key, value }) => {
-      map.set(key, value);
-    });
-    return map;
   }
 }
