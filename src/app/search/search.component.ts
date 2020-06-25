031e5203005318ea6ac04fd49db80d21ffbbdff0
Zachary Svoboda <zachary.svoboda@capco.com>
2020-06-24 21:30:32 -0400

    Fix spinner on loading page

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { SearchService } from './services/search.service';
 import { Observable, BehaviorSubject } from 'rxjs';
-import { switchMap, map, distinctUntilChanged, tap, shareReplay, filter } from 'rxjs/operators';
+import { switchMap, map, distinctUntilChanged, tap, shareReplay, filter, startWith } from 'rxjs/operators';
 import { FormBuilder } from '@angular/forms';
 import { IListItem } from '../shared/components/podcast-list/podcast-list.component';
 import { Router, ActivatedRoute } from '@angular/router';
@@ -16,22 +16,18 @@ import { IPodcast } from '../shared/models/podcast.model';
 export class SearchComponent {
   private loadingBS = new BehaviorSubject(false);
   public loading$ = this.loadingBS.asObservable();
-  public searchResults$: Observable<IPodcast[]> = this.route.queryParamMap.pipe(
+  public searchResults$: Observable<IPodcast[] | boolean> = this.route.queryParamMap.pipe(
     map(qParamMap => qParamMap.get('terms')),
     filter((terms): terms is string => !!terms),
     tap(term => this.searchForm.controls.terms.setValue(term)),
     distinctUntilChanged(),
     switchMap(searchTerms => this.searchService.appleSearch(searchTerms)),
+    // True is a truthy value that clearly shouldn't be evalutated as a list.
+    // We want the nested spinner to be subscribed
+    // Would you rather manage the subscription or have this strangeness?
+    startWith(true),
     tap(() => this.loadingBS.next(false)),
     shareReplay()
-  );
-  public list$ = this.searchResults$.pipe(
-    map<IPodcast[], IListItem[]>(results => results.map(
-      result => ({
-        title: result.name,
-        image: result.thumbnail ? result.thumbnail.medium : undefined
-      })
-    )),
   );
 
   public searchForm = this.fb.group({
@@ -56,5 +52,15 @@ export class SearchComponent {
   public viewPodcast(index: number, currentResults: IPodcast[]): void {
     const key = this.store.setPodcast(currentResults[index]);
     this.router.navigate(['podcast', 'overview', key]);
+  }
+
+  toList(results: IPodcast[] | true, search?: string): IListItem[] | undefined {
+    if(results === true || search === undefined || search.length === 0) return;
+    return results.map(
+      result => ({
+        title: result.name,
+        image: result.thumbnail ? result.thumbnail.medium : undefined
+      })
+    )
   }
 }
