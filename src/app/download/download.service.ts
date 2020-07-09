661173f18259624a17e11a0430271bd060a0bad6
Zachary Svoboda <zachary.svoboda@capco.com>
2020-07-08 21:59:00 -0400

    Return back to the dinky http client, whatever

--- src/app/download/download.service.ts
+++ src/app/download/download.service.ts
@@ -1,78 +1,53 @@
 import { Injectable } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
+import { Observable, Subject, of } from 'rxjs';
 import { IPodcastEpisode } from '../shared/models/podcast.model';
 import { StoreService } from '../store/store.service';
+import { HttpClient, HttpRequest, HttpEventType } from '@angular/common/http';
+import { tap, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DownloadService {
-  constructor(private store: StoreService) {}
+  constructor(private store: StoreService, private http: HttpClient) {}
 
   public downloadEpisode(episode: IPodcastEpisode): Observable<IDownloadReport> {
-    const progressReporter = new Subject<IDownloadReport>();
-    const downloader = this.buildDownloader(episode.audioUrl);
-    this.download(downloader, progressReporter).then(audio => {
-      this.store.setEpisode({...episode, audio});
-      progressReporter.next(new DownloadProgress(1, 1, true));
-    }).catch(fail => {
-      progressReporter.next({
-        reason: 'cors',
-        failure: true
-      });
-    });
-    return progressReporter.asObservable();
-  }
-
-  private async download(
-    downloader: AsyncGenerator<DownloadProgress | Blob>,
-    sub: Subject<IDownloadReport>
-  ): Promise<Blob | undefined> {
-    for await(const progress of downloader) {
-      if(progress instanceof DownloadProgress) {
-        sub.next(progress);
-      } else {
-        return progress;
-      }
-    }
-  }
-
-  private async* buildDownloader(url: string): AsyncGenerator<DownloadProgress | Blob> {
-    const response = await fetch(url);
-
-    if(response.body) {
-      const contentLength: string = response.headers.get('Content-Length') || String(Number.MAX_SAFE_INTEGER);
-      const total = Number.parseInt(contentLength, 10);
-      const iterableReader = this.buildIterableReader<Uint8Array>(response.body.getReader());
-      let receivedLength = 0;
-      const chunks: Uint8Array[] = [];
-
-      for await (const {value} of iterableReader) {
-        if(value !== undefined) {
-          chunks.push(value);
-          receivedLength += value.length;
-          yield new DownloadProgress(receivedLength, total);
+    const sub = new Subject<IDownloadReport>();
+    const req = new HttpRequest('GET', episode.audioUrl, { reportProgress: true, responseType: 'blob' } );
+    this.http.request<Blob>(req).pipe(
+      tap(event => {
+        switch (event.type) {
+          case HttpEventType.DownloadProgress:
+            sub.next(new DownloadProgress(event.loaded, event.total, false));
+          break;
+          case HttpEventType.Response:
+            if(event.body) {
+              this.store.setEpisode({...episode, audio: event.body});
+              sub.next(new DownloadProgress(1, 1, true));
+            } else {
+              sub.next({
+                failure: true,
+                reason: 'unknown'
+              });
+            }
+            break;
+          default:
+            sub.next({
+              failure: true,
+              reason: event.type.toString()
+            });
         }
-      }
+      }),
+      catchError(err => {
+        sub.next({
+          failure: true,
+          reason: err
+        });
+        return of();
+      })
+    ).subscribe();
 
-      const chunksAll = new Uint8Array(receivedLength);
-      let position = 0;
-      for(const chunk of chunks) {
-        chunksAll.set(chunk, position);
-        position += chunk.length;
-      }
-      yield new Blob([chunksAll]);
-
-    }
-  }
-
-  private async* buildIterableReader<T>(reader: ReadableStreamDefaultReader<T>): AsyncGenerator<ReadableStreamReadResult<T>> {
-    let finished = false;
-    while(!finished) {
-      const red = await reader.read();
-      finished = red.done;
-      yield red;
-    }
+    return sub;
   }
 }
 
--- src/app/podcast/presentations/episode/episode.component.ts
+++ src/app/podcast/presentations/episode/episode.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ActivatedRoute, Router } from '@angular/router';
-import { map, filter, share, switchMap } from 'rxjs/operators';
+import { map, filter, share, switchMap, tap } from 'rxjs/operators';
 import { PodcastService } from 'src/app/shared/services/podcast.service';
 import { IPodcastEpisode } from 'src/app/shared/models/podcast.model';
 import { PlaylistService } from 'src/app/playlist/services/playlist.service';
@@ -44,6 +44,6 @@ export class EpisodeComponent {
   }
 
   public downloadEpisode(episode: IPodcastEpisode): void {
-    this.downloadProgress = this.downloadService.downloadEpisode(episode);
+    this.downloadProgress = this.downloadService.downloadEpisode(episode).pipe(tap(console.log));
   }
 }
