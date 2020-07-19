813725c8d1f2a4e08a023c757ee1b3dfbf4688fc
Zachary Svoboda <zachary.svoboda@capco.com>
2020-07-18 20:40:58 -0400

    Move podcast components

--- src/app/podcast/podcast.module.ts
+++ src/app/podcast/podcast.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { OverviewModule } from './presentations/overview/overview.module';
 import { Routes, RouterModule } from '@angular/router';
-import { OverviewComponent } from './presentations/overview/overview.component';
-import { FeedComponent } from './presentations/feed/feed.component';
-import { FeedModule } from './presentations/feed/feed.module';
-import { EpisodeModule } from './presentations/episode/episode.module';
-import { EpisodeComponent } from './presentations/episode/episode.component';
+import { OverviewComponent } from './overview/overview.component';
+import { FeedComponent } from './feed/feed.component';
+import { EpisodeComponent } from './episode/episode.component';
+import { OverviewModule } from './overview/overview.module';
+import { FeedModule } from './feed/feed.module';
+import { EpisodeModule } from './episode/episode.module';
 
 
 const routes: Routes = [
